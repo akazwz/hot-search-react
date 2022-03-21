a0db186d492cafd7816bb4781f90af6e2a5f0960
akazwz <akazwz@pm.me>
2022-03-21 14:07:10 +0800

    update packages

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,10 +6,10 @@ export const Hero = (props: { searches: ISearch[] }) => {
   return (
     <Box>
       <Box display={{ base: 'flex', md: 'none' }}>
-        <WordCloudHot searches={props.searches} width={350} height={350}/>
+        <WordCloudHot searches={props.searches} width={400} height={350} />
       </Box>
       <Box display={{ base: 'none', md: 'flex' }}>
-        <WordCloudHot searches={props.searches} width={700} height={350}/>
+        <WordCloudHot searches={props.searches} width={700} height={350} />
       </Box>
     </Box>
   )
